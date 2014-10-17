f8af8344c6bd74e3e3a0247d46193e13f3a7e069
Jason Barron <jason@cutehacks.com>
2014-10-17 15:04:03 +0200

    Run make install to get all the libraries in the same place

---
+++ androidqtdev/symbolize.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import glob
+import argparse
+from os.path import isfile
+from os.path import exists
+
+SYMBOLS_DIR = "/opt/symbols/"
+
+def symbolize(filename):
+  symbolFilename = SYMBOLS_DIR + filename + ".sym"
+  os.system("dump_syms " + filename + " > " + symbolFilename)
+  symbolFile = open(symbolFilename, 'r')
+  module = symbolFile.readline().split(" ")
+  moduleId =  module[3]
+  moduleName = SYMBOLS_DIR + "/" + moduleId + "/" + module[4].strip()
+
+  if not exists(moduleName):
+    os.makedirs(moduleName)
+
+  os.rename(symbolFilename, moduleName + "/" + filename + ".sym")
+
+
+if not exists(SYMBOLS_DIR):
+  os.mkdir(SYMBOLS_DIR)
+
+if len(sys.argv) > 1:
+  target = sys.argv[1]
+  if (isfile(target)):
+      symbolize(target)
+  else:
+    for filename in glob.glob(target + '/*.so'):
+      symbolize(filename)
+else:
+  for filename in glob.glob('*.so'):
+    symbolize(filename)
+
